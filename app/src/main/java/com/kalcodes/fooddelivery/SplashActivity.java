8e47f3e170bec4631bdfc72efc69edda991bf52d
kaleab <kidusharar@gmail.com>
2022-10-06 14:48:50 +0300

    Check If User is Already Logged In

--- app/src/main/java/com/kalcodes/fooddelivery/SplashActivity.java
+++ app/src/main/java/com/kalcodes/fooddelivery/SplashActivity.java
@@ -7,24 +7,26 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class SplashActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT=3000;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        mAuth = FirebaseAuth.getInstance();
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-//                SharedPreferences sharedPreferences = getSharedPreferences(LoginActivity.PREFS_NAME,0);
-                SharedPreferences sharedPreferences = getSharedPreferences(LoginActivity.PRINT_SERVICE,0);
-                Boolean hasLoggedIn = sharedPreferences.getBoolean("hasLoggedIn",false);
 
-                if (hasLoggedIn){
+                if (mAuth.getCurrentUser() != null){
                     Intent intent = new Intent(SplashActivity.this,MainActivity.class);
                     startActivity(intent);
                     finish();
