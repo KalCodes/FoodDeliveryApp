2e48195a1c3b46970fcb4acb0334709b49eb5c91
kaleab <kidusharar@gmail.com>
2022-10-07 14:35:00 +0300

    Image slider with Firebase Database

--- app/src/main/java/com/kalcodes/fooddelivery/HomeFragment.java
+++ app/src/main/java/com/kalcodes/fooddelivery/HomeFragment.java
@@ -4,14 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     public HomeFragment() {
         // Required empty public constructor
     }
+    ImageSlider imageSlider;
     View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,7 +34,29 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        getActivity().setTitle("Home");
 
+        getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        imageSlider = view.findViewById(R.id.imageSlider);
+        final List<SlideModel> remoteimages = new ArrayList<>();
+
+        FirebaseDatabase.getInstance().getReference().child("Slider")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for(DataSnapshot data:dataSnapshot.getChildren()){
+                            remoteimages.add(new SlideModel(data.child("url").getValue().toString(),data.child("title").getValue().toString(), ScaleTypes.FIT ));
+
+                            imageSlider.setImageList(remoteimages,ScaleTypes.FIT);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
 
         return view;
     }
